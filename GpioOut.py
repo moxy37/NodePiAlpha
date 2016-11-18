4882636d9e49bb3fa020798016562e5abd983e6e
dbrennan <dean.brennan@fglsports.com>
2016-11-18 15:10:02 -0700

    Pushing for fun

--- GpioOut.py
+++ GpioOut.py
@@ -1,12 +1,14 @@
-import RPi.GPIO as g
+#import RPi.GPIO as g
+import Adafruit_BBIO.GPIO as g
 import time
 import sys
 
-pin = int(sys.argv[1])
+#pin = int(sys.argv[1])
+pin = sys.argv[1]
 status = sys.argv[2]
 
-g.setmode(g.BOARD)
-g.setwarnings(False)
+#g.setmode(g.BOARD)
+#g.setwarnings(False)
 g.setup(pin, g.OUT)
 if str(status) == "1":
 	g.output(pin, g.HIGH)
