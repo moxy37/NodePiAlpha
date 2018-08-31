1029397f1d541cfcfb4155a649c6bbe426f54789
moxy37 <dbrennan@nu-worx.com>
2018-08-30 21:24:39 -0600

    Trying to push code for Ascii.py

--- CowListener.py
+++ CowListener.py
@@ -15,23 +15,25 @@ temp1 = '37.2'
 temp2 = '37.4'
 cowid = '1'
 rssi = '0'
-ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
 while True:
-    print "Reading"
     while output != "":
-        output = ser.readline()
-        data = output.split(':')
-        latitude = data[0]
-        longitude = data[1]
-        rssi = data[2]
-        cowid = data[3]
-        temp1 = data[4]
-        temp2 = data[5]
-        output2 = latitude + '_' + longitude + '_' + rssi + '_' + cowid + '_' + temp1 + '_' + temp2
-        print(output)
         try:
-            some_url = url + '/api/cow/log/' + output2
-            f = urllib2.urlopen(some_url)
-            print f.read()
+            output = ser.readline()
+            data = output.split(':')
+            latitude = data[0]
+            longitude = data[1]
+            rssi = data[2]
+            cowid = data[3]
+            temp1 = data[4]
+            temp2 = data[5]
+            output2 = latitude + '_' + longitude + '_' + rssi + '_' + cowid + '_' + temp1 + '_' + temp2
+            print(output2)
+            try:
+                some_url = url + '/api/cow/log/' + output2
+                f = urllib2.urlopen(some_url)
+                print f.read()
+            except:
+                pass
         except:
-            pass
+            print("Error")
